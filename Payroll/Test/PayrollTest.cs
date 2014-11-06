2232e2bbfb765d3b17558b7830c588e6d248900c
liucailin <liu1356@gmail.com>
2014-11-07 01:35:53 +0800

    Add HourlyEmployee Transaction

--- Payroll/Test/PayrollTest.cs
+++ Payroll/Test/PayrollTest.cs
@@ -26,10 +26,22 @@ namespace Payroll.Test
 
 			PaymentMethod pm = e.Method;
 			Assert.IsTrue(pm is HoldMethod);
+		}
 
 
+		[Test]
+		public void TestAddHourlyEmployee ()
+		{
+			int empId = 2;
+			AddEmployeeTransaction t = new AddHourlyEmployee(empId, "Lucy", "home", 9.5f);
+			t.Execute();
 
+			Employee e = PayrollDatabase.GetEmployee(empId);
+			Assert.AreEqual("Lucy", e.Name);
 
+			Assert.IsTrue(e.Classification is HourlyClassification);
+			Assert.IsTrue(e.Schedule is WeeklySchedule);
+			Assert.AreEqual(9.5f, (e.Classification as HourlyClassification).Hourly, .001);
 		}
 	}
 }
