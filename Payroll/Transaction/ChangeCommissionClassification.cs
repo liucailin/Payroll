b9355bdc7e577bed9be7b8496d36fdbebaa09abb
liucailin <liu1356@gmail.com>
2014-11-10 11:57:02 +0800

    * ChangeCommissionClassification.cs: Change CommissionClassification
    * ChangeSalariedClassification.cs: Change SalariedClassification

---
+++ Payroll/Transaction/ChangeCommissionClassification.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Payroll
+{
+    class ChangeCommissionClassification : ChangeClassificationTransaction
+    {
+        private float percent;
+
+        public ChangeCommissionClassification(int empid, float percent) : base (empid)
+        {
+            this.percent = percent;
+        }
+
+        protected override PaymentClassification GetClassification()
+        {
+            return new CommissionClassification(percent);
+        }
+
+        protected override PaymentSchedule GetSchedule()
+        {
+            return new BiweeklySchedule();
+        }
+    }
+}
+
---
+++ Payroll/Transaction/ChangeSalariedClassification.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Payroll
+{
+    class ChangeSalariedClassification : ChangeClassificationTransaction
+    {
+        private double salary;
+
+        public ChangeSalariedClassification(int empid, double salary) : base (empid)
+        {
+            this.salary = salary;
+        }
+
+        protected override PaymentClassification GetClassification()
+        {
+            return new SalariedClassification(salary);
+        }
+
+        protected override PaymentSchedule GetSchedule()
+        {
+            return new MonthlySchedule();
+        }
+    }
+}
+
